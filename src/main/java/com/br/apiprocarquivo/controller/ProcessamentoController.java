52fd86cd98faad123dcc32d60ee53e2413c82228
Paulo Ruszel <paulo.luz-terceiro@cea.com.br>
2024-02-23 20:43:01 -0300

    ajuste nos imports

--- src/main/java/com/br/apiprocarquivo/controller/ProcessamentoController.java
+++ src/main/java/com/br/apiprocarquivo/controller/ProcessamentoController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 import static com.br.apiprocarquivo.domain.enums.ProcessamentoStatus.AGUARDANDO;
